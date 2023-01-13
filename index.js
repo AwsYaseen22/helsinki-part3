6bc78d97d8df9bb42b9757649313f78544839fbb
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-13 15:36:43 +0300

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -4,37 +4,42 @@ const app = express();
 app.use(express.json());
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 app.get("/", (request, response) => {
   response.send("<h1>Phone Book</h1>");
 });
 
+app.get("/info", (request, response) => {
+  response.send(`
+    <h3>Phonebook has infor for ${persons.length} people</h3>
+    <h3>${new Date()}</h3>
+  `);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
-
-
 
 const PORT = 3001;
 app.listen(PORT, () => {
