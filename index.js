19a7305fe5e5be93eb995566f7467dbf96cf6e66
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-14 23:37:05 +0300

    preparing for deploy

--- index.js
+++ index.js
@@ -36,15 +36,6 @@ let persons = [
   },
 ];
 
-// const requestLogger = (request, response, next) => {
-//   console.log("Method:", request.method);
-//   console.log("Path:  ", request.path);
-//   console.log("Body:  ", request.body);
-//   console.log("---");
-//   next();
-// };
-// app.use(requestLogger);
-
 app.get("/", (request, response) => {
   response.send("<h1>Phone Book</h1>");
 });
@@ -107,7 +98,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
