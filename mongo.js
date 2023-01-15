3952cd689ae50ccbe9b419af7c03d72e28fa1500
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-16 00:25:51 +0300

    3.12: Command-line database

--- mongo.js
+++ mongo.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+let create = false;
+
 if (process.argv.length < 3) {
   console.log(
     "Please provide the password as an argument: node mongo.js <password>"
@@ -7,40 +9,45 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
+if (process.argv.length === 5) {
+  create = true;
+}
+
 const password = process.argv[2];
 
-const url = `mongodb+srv://AwsYaseen22:${password}@part3-c.13dowqj.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://AwsYaseen22:${password}@part3-c.13dowqj.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Person = mongoose.model("Person", personSchema);
 
 mongoose
   .connect(url)
   .then((result) => {
     console.log("connected");
+    if (create) {
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      });
 
-    // // create new record
-    // const note = new Note({
-    //   content: "HTML is Easy 3",
-    //   date: new Date(),
-    //   important: true,
-    // });
-
-    // return note.save();
-
-    return Note.find({});
+      return person.save();
+    } else {
+      return Person.find({});
+    }
   })
   .then((result) => {
-    console.log("the result: ");
-    result.forEach((note) => {
-      console.log(note);
-    });
-
+    if (create) {
+      console.log(`added ${result.name} number ${result.number} to phonebook`);
+    } else {
+      console.log("phonebook:");
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+    }
     return mongoose.connection.close();
   })
   .catch((err) => console.log(err));
