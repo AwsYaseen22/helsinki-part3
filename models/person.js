8c6c382a92ab2a492760874363b14cdf779434cf
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-18 00:07:43 +0300

    3.20*: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 const personSchema = mongoose.Schema({
-  name: { type: String, minLength: 3, required: true },
-  number: { type: String, required: true },
+  name: { type: String, minLength: 3, required: true, unique: true },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, "Phone number required"],
+    validate: {
+      validator: (v) => {
+        return /\d{2,3}-\d{1,}/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! shoudl be formated as 123-456789*`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
